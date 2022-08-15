62aa92c842f21572a5f11d3d5708a888ed62432d
AzureKn1ght <philsonnah@msn.com>
2022-08-15 16:01:30 +0800

    Updated comments

--- hardhat/contracts/CryptoDevToken.sol
+++ hardhat/contracts/CryptoDevToken.sol
@@ -8,17 +8,21 @@ import "./ICryptoDevs.sol";
 contract CryptoDevToken is ERC20, Ownable {
     // Price of one Crypto Dev token
     uint256 public constant tokenPrice = 0.001 ether;
+
     // Each NFT would give the user 10 tokens
     // It needs to be represented as 10 * (10 ** 18) as ERC20 tokens are represented by the smallest denomination possible for the token
     // By default, ERC20 tokens have the smallest denomination of 10^(-18). This means, having a balance of (1)
     // is actually equal to (10 ^ -18) tokens.
     // Owning 1 full token is equivalent to owning (10^18) tokens when you account for the decimal places.
     // More information on this can be found in the Freshman Track Cryptocurrency tutorial.
-    uint256 public constant tokensPerNFT = 10 * 10**18;
+    uint256 public constant tokensPerNFT = 10 * 10**18; //can use '10 * 1 ether' too
+
     // the max total supply is 10000 for Crypto Dev Tokens
     uint256 public constant maxTotalSupply = 10000 * 10**18;
+
     // CryptoDevsNFT contract instance
     ICryptoDevs CryptoDevsNFT;
+
     // Mapping to keep track of which tokenIds have been claimed
     mapping(uint256 => bool) public tokenIdsClaimed;
 
@@ -53,13 +57,16 @@ contract CryptoDevToken is ERC20, Ownable {
      */
     function claim() public {
         address sender = msg.sender;
+
         // Get the number of CryptoDev NFT's held by a given sender address
         uint256 balance = CryptoDevsNFT.balanceOf(sender);
         // If the balance is zero, revert the transaction
         require(balance > 0, "You dont own any Crypto Dev NFT's");
-        // amount keeps track of number of unclaimed tokenIds
+        
+        // amount keeps track of number of unclaimed tokenIds for this user
         uint256 amount = 0;
-        // loop over the balance and get the token ID owned by `sender` at a given `index` of its token list.
+
+        // loop over the balance and get the token ID owned by `sender` at a given `index` of its token list. Basically, for each token he holds, what's the ID
         for (uint256 i = 0; i < balance; i++) {
             uint256 tokenId = CryptoDevsNFT.tokenOfOwnerByIndex(sender, i);
             // if the tokenId has not been claimed, increase the amount
@@ -68,10 +75,12 @@ contract CryptoDevToken is ERC20, Ownable {
                 tokenIdsClaimed[tokenId] = true;
             }
         }
+
         // If all the token Ids have been claimed, revert the transaction;
         require(amount > 0, "You have already claimed all the tokens");
+        
         // call the internal function from Openzeppelin's ERC20 contract
-        // Mint (amount * 10) tokens for each NFT
+        // Mint (amount * 10) tokens for each NFT - "tokensPerNFT" is the unit
         _mint(msg.sender, amount * tokensPerNFT);
     }
 
